86a577719688b3415a861b8b6ca5e054577866c5
Shrouk Sobhy <shroukksobhy@gmail.com>
2024-08-24 02:49:14 +0300

    Add group of auth

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+        return $request->user();
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
+use App\Http\Controllers\API\UserController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/user', [UserController::class, 'index']);
+
 });
